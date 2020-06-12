91c19c2a4fbaf5312ef1300f03083f33a3152df4
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-06-11 19:50:08 -0600

    Funcionalidad

--- lab8sensores/app/src/main/java/com/example/lab8sensores/ContactActivity.java
+++ lab8sensores/app/src/main/java/com/example/lab8sensores/ContactActivity.java
@@ -1,14 +1,134 @@
 package com.example.lab8sensores;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
+
+    ImageView photo;
+    ImageButton callBtn, smsBtn, cameraBtn, volverBtn,sendBtn;
+    EditText phone, name,message;
+    private static final int Image_Capture_Code = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},1);
+
+        photo = (ImageView) findViewById(R.id.photo);
+
+        callBtn = (ImageButton) findViewById(R.id.callBtn);
+        smsBtn = (ImageButton) findViewById(R.id.smsBtn);
+        cameraBtn = (ImageButton) findViewById(R.id.cameraBtn);
+        volverBtn = (ImageButton) findViewById(R.id.volverBtn);
+        sendBtn = (ImageButton) findViewById(R.id.sendBtn);
+
+        phone = (EditText) findViewById(R.id.phoneNumber);
+        name = (EditText) findViewById(R.id.nameText);
+        message = (EditText) findViewById(R.id.messageContent);
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cInt, Image_Capture_Code);
+            }
+        });
+
+        smsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(message.getVisibility()==View.INVISIBLE && sendBtn.getVisibility()==View.INVISIBLE){
+                    message.setVisibility(View.VISIBLE);
+                    sendBtn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(phone.getText().toString(),null,message.getText().toString(),null,null);
+                    Toast.makeText(ContactActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+                    message.setVisibility(View.INVISIBLE);
+                    sendBtn.setVisibility(View.INVISIBLE);
+                }
+                catch (Exception e){
+                    Toast.makeText(ContactActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callPhoneNumber();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                photo.setImageBitmap(bp);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 101) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callPhoneNumber();
+            }
+        }
+    }
+
+    public void callPhoneNumber() {
+        try {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ContactActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phone.getText().toString()));
+                startActivity(callIntent);
+
+            } else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phone.getText().toString()));
+                startActivity(callIntent);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
